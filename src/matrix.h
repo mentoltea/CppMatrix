ecca52d7d4c69db431283397276e31d174ecaeee
s7k-windows <benotimprotant@mail.ru>
2025-03-14 20:09:11 +0300

    interface added

--- src/matrix.h
+++ src/matrix.h
@@ -1,18 +1,50 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <fstream>
+
 class Matrix {
 public:
     unsigned int a, b; // dimensions of matrix
-    double **data;
-
+    
     // @param a Number of rows
     // @param b Number of columns
     // @param initvalue Value to initialize matrix with
     Matrix(unsigned int a, unsigned int b, double initvalue=0);
-    ~Matrix();
-
+    Matrix(std::ifstream fd);
     
+    Matrix(const Matrix& other);
+    Matrix(Matrix&& other);
+    ~Matrix();
+    
+    Matrix& operator=(const Matrix& other);
+    Matrix& operator=(Matrix&& other);
+    
+    
+    Matrix operator*(const Matrix& other);
+    Matrix operator+(const Matrix& other);
+    Matrix operator-(const Matrix& other);
+    
+    Matrix& operator*=(const Matrix& other);
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator-=(const Matrix& other);
+    
+    Matrix operator^(unsigned int n);
+    
+    double det();
+    
+    Matrix inverse();
+
+    Matrix trans();
+
+    unsigned int rank();
+
+    double **get_data();
+
+private:
+    double **data = nullptr;
 };
+
+
 
 #endif // MATRIX_H 
