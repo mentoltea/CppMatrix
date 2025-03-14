77da1d6ecc1a2bd5b594fa4d4960e7a4ff26bc16
s7k-windows <benotimprotant@mail.ru>
2025-03-14 17:19:36 +0300

    Added doxygen documentiser

--- src/matrix.h
+++ src/matrix.h
@@ -6,6 +6,9 @@ public:
     unsigned int a, b; // dimensions of matrix
     double **data;
 
+    // @param a Number of rows
+    // @param b Number of columns
+    // @param initvalue Value to initialize matrix with
     Matrix(unsigned int a, unsigned int b, double initvalue=0);
     ~Matrix();
 
